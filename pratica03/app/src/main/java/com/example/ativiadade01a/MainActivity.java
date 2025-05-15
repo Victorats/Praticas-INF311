064276d28c0aaaf046f47a5da00e42ecf45a989b
Victorats <victorats@outlook.com>
2025-05-15 12:18:25 -0300

    passar coordenadas pra segunda tela

--- pratica03/app/src/main/java/com/example/ativiadade01a/MainActivity.java
+++ pratica03/app/src/main/java/com/example/ativiadade01a/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ativiadade01a;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -21,14 +22,32 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         String menu[] = new String[] {"Minha casas em Teofilo Otoni", "Minha casa em Viçosa", "DPI UFV", "Fechar aplicativo"};
         ArrayAdapter<String> arrAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
-        setListAdapter(arrAdapt);
+            setListAdapter(arrAdapt);
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         String aux = l.getItemAtPosition(position).toString();
         Intent it = new Intent(this, MostraMapaActivity.class);
-        it.putExtra("option", aux);
+        if(aux.equals("Fechar aplicativo")){
+            finish();
+        }
+        Pair<Double, Double> coord = retornaCoordernada(aux);
+        it.putExtra("x",coord.first);
+        it.putExtra("y",coord.second);
         startActivity(it);
+    }
+
+    protected android.util.Pair<Double, Double> retornaCoordernada(String local){
+        if(local == "Minha casas em Teofilo Otoni"){
+            return new Pair<>(-17.854417, -41.508195);
+        }
+        else if(local == "Minha casa em Viçosa"){
+            return new Pair<>(-20.755195831967093, -42.876022587753255);
+        }
+        else if(local == "DPI UFV"){
+            return new Pair<>(-20.76482710197161, -42.86845007536529);
+        }
+        return null;
     }
 }
